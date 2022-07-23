32bbfb96402ace5c87b1391e6bb6728ab2199c8e
Forwall100 <hezeri546@gmail.com>
2022-07-23 13:01:39 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,178 @@
+<template>
+  <header class="header">
+    <a href="#" class="header__logo">Eremey Ikashev</a>
+    <nav class="header__nav">
+      <ul class="header__list">
+        <li class="header__item"><a href="#" class="header__link">Portfolio</a></li>
+        <li class="header__item"><a href="https://t.me/namename5" class="header__link">Contact me</a></li>
+      </ul>
+    </nav>
+    <button v-if="lightMode" @click="lightMode = !lightMode" class="sun__button">
+      <svg xmlns="http://www.w3.org/2000/svg" class="sun_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+        stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+      </svg>
+    </button>
+    <button v-if="lightMode == false" @click="lightMode = !lightMode" class="moon__button">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#f0e7db"
+        stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+      </svg>
+    </button>
+  </header>
+  <section>
+    <div class="container">
+      <div class="main">
+        <div class="main__text">
+          <h2 class="main__title">Eremey Ikashev</h2>
+          <p class="main__desc">3D artist, Designer, Developer</p>
+        </div>
+        <div class="main__image">
+          <img class="main__img" src="./assets/images/avatar.png" alt="avatar">
+        </div>
+      </div>
+      <div class="article">
+        <div class="article__paragraph">
+          <h3 class="article__title">Links</h3>
+          <ul class="article__desc">
+            <li class="article__desc__list__item">
+              <a class="article__desc__list__item__button" href="https://www.behance.net/hezeri5467e23/projects">
+                <svg class="item__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                  <path
+                    d="M232 237.2c31.8-15.2 48.4-38.2 48.4-74 0-70.6-52.6-87.8-113.3-87.8H0v354.4h171.8c64.4 0 124.9-30.9 124.9-102.9 0-44.5-21.1-77.4-64.7-89.7zM77.9 135.9H151c28.1 0 53.4 7.9 53.4 40.5 0 30.1-19.7 42.2-47.5 42.2h-79v-82.7zm83.3 233.7H77.9V272h84.9c34.3 0 56 14.3 56 50.6 0 35.8-25.9 47-57.6 47zm358.5-240.7H376V94h143.7v34.9zM576 305.2c0-75.9-44.4-139.2-124.9-139.2-78.2 0-131.3 58.8-131.3 135.8 0 79.9 50.3 134.7 131.3 134.7 61.3 0 101-27.6 120.1-86.3H509c-6.7 21.9-34.3 33.5-55.7 33.5-41.3 0-63-24.2-63-65.3h185.1c.3-4.2.6-8.7.6-13.2zM390.4 274c2.3-33.7 24.7-54.8 58.5-54.8 35.4 0 53.2 20.8 56.2 54.8H390.4z" />
+                </svg>
+                Behance
+              </a>
+            </li>
+            <li class="article__desc__list__item">
+              <a class="article__desc__list__item__button" href="https://github.com/Forwall100">
+                <svg class="item__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                  <path
+                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                </svg>
+                Github
+              </a>
+            </li>
+            <li class="article__desc__list__item">
+              <a class="article__desc__list__item__button" href="https://dribbble.com/beeming">
+                <svg class="item__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                  <path
+                    d="M256 8C119.252 8 8 119.252 8 256s111.252 248 248 248 248-111.252 248-248S392.748 8 256 8zm163.97 114.366c29.503 36.046 47.369 81.957 47.835 131.955-6.984-1.477-77.018-15.682-147.502-6.818-5.752-14.041-11.181-26.393-18.617-41.614 78.321-31.977 113.818-77.482 118.284-83.523zM396.421 97.87c-3.81 5.427-35.697 48.286-111.021 76.519-34.712-63.776-73.185-116.168-79.04-124.008 67.176-16.193 137.966 1.27 190.061 47.489zm-230.48-33.25c5.585 7.659 43.438 60.116 78.537 122.509-99.087 26.313-186.36 25.934-195.834 25.809C62.38 147.205 106.678 92.573 165.941 64.62zM44.17 256.323c0-2.166.043-4.322.108-6.473 9.268.19 111.92 1.513 217.706-30.146 6.064 11.868 11.857 23.915 17.174 35.949-76.599 21.575-146.194 83.527-180.531 142.306C64.794 360.405 44.17 310.73 44.17 256.323zm81.807 167.113c22.127-45.233 82.178-103.622 167.579-132.756 29.74 77.283 42.039 142.053 45.189 160.638-68.112 29.013-150.015 21.053-212.768-27.882zm248.38 8.489c-2.171-12.886-13.446-74.897-41.152-151.033 66.38-10.626 124.7 6.768 131.947 9.055-9.442 58.941-43.273 109.844-90.795 141.978z" />
+                </svg>
+                Dribble
+              </a>
+            </li>
+            <li class="article__desc__list__item">
+              <a class="article__desc__list__item__button" href="https://vk.com/kolkov96">
+                <svg class="item__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                  <path
+                    d="M31.4907 63.4907C0 94.9813 0 145.671 0 247.04V264.96C0 366.329 0 417.019 31.4907 448.509C62.9813 480 113.671 480 215.04 480H232.96C334.329 480 385.019 480 416.509 448.509C448 417.019 448 366.329 448 264.96V247.04C448 145.671 448 94.9813 416.509 63.4907C385.019 32 334.329 32 232.96 32H215.04C113.671 32 62.9813 32 31.4907 63.4907ZM75.6 168.267H126.747C128.427 253.76 166.133 289.973 196 297.44V168.267H244.16V242C273.653 238.827 304.64 205.227 315.093 168.267H363.253C359.313 187.435 351.46 205.583 340.186 221.579C328.913 237.574 314.461 251.071 297.733 261.227C316.41 270.499 332.907 283.63 346.132 299.751C359.357 315.873 369.01 334.618 374.453 354.747H321.44C316.555 337.262 306.614 321.61 292.865 309.754C279.117 297.899 262.173 290.368 244.16 288.107V354.747H238.373C136.267 354.747 78.0267 284.747 75.6 168.267Z" />
+                </svg>
+                VK
+              </a>
+            </li>
+            <li class="article__desc__list__item">
+              <a class="article__desc__list__item__button" href="https://t.me/namename5">
+                <svg class="item__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                  <path
+                    d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z" />
+                </svg>
+                Telegram
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="article__paragraph">
+          <h3 class="article__title">About me</h3>
+          <p class="article__desc">
+            &emsp; I am a designer and developer from Belarus who wants to develop incredible products.
+            Currently,
+            I live off freelancing and study technologies that are interesting to me.
+          </p>
+        </div>
+        <div class="article__paragraph">
+          <h3 class="article__title">Bio</h3>
+          <ul class="article__desc">
+            <li class="article__desc__list__item"><b>2002</b> Born in Gomel, Belarus</li>
+            <li class="article__desc__list__item"><b>2014</b> I studied Photoshop and started doing previews
+              for
+              YouTubers</li>
+            <li class="article__desc__list__item"><b>2015</b> I'm starting to learn python and write
+              telegram bots
+            </li>
+            <li class="article__desc__list__item"><b>2019</b> I am entering the university in the direction
+              of
+              software development</li>
+            <li class="article__desc__list__item"><b>2020 - today</b> Still learning and living off
+              freelancing
+            </li>
+          </ul>
+        </div>
+        <div class="article__paragraph">
+          <h3 class="article__title">I love</h3>
+          <p class="article__desc">
+            Open source, art, blockchain, money, drawing, myself
+          </p>
+        </div>
+
+      </div>
+      <br>
+      <footer>
+        © 2022 Eremey Ikashev. All Rights Reserved.
+      </footer>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      lightMode: false,
+    }
+  },
+  mounted() {
+    // set page title
+    document.title = 'Multiple Themes in Vue.js';
+
+    // set 'app-background' class to body tag
+    let bodyElement = document.body;
+    bodyElement.classList.add("app-background");
+
+    // check for active theme
+    let htmlElement = document.documentElement;
+    let theme = localStorage.getItem("theme");
+
+    if (theme === 'dark') {
+      htmlElement.setAttribute('theme', 'dark')
+      this.lightMode = true
+    } else {
+      htmlElement.setAttribute('theme', 'light');
+      this.lightMode = false
+    }
+  },
+  watch: {
+    lightMode: function () {
+      // add/remove class to/from html tag
+      let htmlElement = document.documentElement;
+
+      if (this.lightMode) {
+        localStorage.setItem("theme", 'dark');
+        htmlElement.setAttribute('theme', 'dark');
+      } else {
+        localStorage.setItem("theme", 'light');
+        htmlElement.setAttribute('theme', 'light');
+      }
+    }
+  }
+}
+</script>
